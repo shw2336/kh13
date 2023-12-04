49a9045a21e1211282edaf3e90277e2ce877cdd6
user1 <user1@DESKTOP-BC1M4DD>
2023-12-04 18:16:56 +0900

    첫 프로젝트 생성

---
+++ welcome/src/test/Hello.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!!");
+		System.out.println("안녕하세요!");
+		System.out.println("반갑습니다!!!");
+		System.out.println("만나서 반갑습니다!");
+	}
+}
