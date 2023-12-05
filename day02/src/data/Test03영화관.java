9a734db65b3dbd141e1b7acf375bb07ec9cbbdcf
shw2336 <shw2336@naver.com>
2023-12-05 14:54:38 +0900

    Test03 영화관 문제계산 문제풀이

--- day02/src/data/Test03영화관.java
+++ day02/src/data/Test03영화관.java
@@ -3,17 +3,20 @@ package data;
 public class Test03영화관 {
 	public static void main(String[] args) {
 		
-		int adult = 12000;
-		int teenager = 8500;
-		int adultPopulation = 2;
-		int teenagerPopulation = 2;
+		//입력 - 문제에서 주어진 값들을 변수에 저장
+		int adultPrice = 12000;
+		int teenagerPrice = 8500;
+		int adultCount = 2;
+		int teenagerCount = 2;
 		
-		int adultTotal = adult * adultPopulation;
-		int teenagerTotal = teenager * teenagerPopulation;
+		//계산 
+		int adultTotal = adultPrice * adultCount;
+		int teenagerTotal = teenagerPrice * teenagerCount;
 		int total = adultTotal + teenagerTotal;
 		
-		System.out.println(adultTotal);
-		System.out.println(teenagerTotal);
+		//출력 - 문제에서 요구하는 값을 화면에 출력
+		//System.out.println(adultTotal);
+		//System.out.println(teenagerTotal);
 		System.out.println(total);
 	}
 }
