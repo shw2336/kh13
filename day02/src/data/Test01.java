357fbe73a5d20a2fa9bd0d374650e686ff74143c
shw2336 <shw2336@naver.com>
2023-12-05 13:23:59 +0900

    Test 01 배움

---
+++ day02/src/data/Test01.java
@@ -0,0 +1,20 @@
+package data;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//데이터(data)
+		//- 측정된 값(수치)
+		//- (ex) 영등포구 당산동 미세먼지 농도
+		//정보(information)
+		//- 데이터를 가공해서 만든 결과물
+		//- (ex) 올해 전국 평균 미세먼지 농도
+		
+		System.out.println("2023-12-05");// 글자를 출력
+		System.out.println(2023-12-05);//식의 결과를 출력
+		
+		//(Q) 식권이 한장에 7000원일 때 식권 10장은 얼마?
+		//System.out.println(70000); //나의 재롱잔치 ㅎ
+		System.out.println(7000 * 10); //컴퓨터에게 시킨 것
+
+	}
+}
