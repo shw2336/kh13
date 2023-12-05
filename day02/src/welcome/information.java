634ed627bf3a0735b650ecf905c428413dfc999d
shw2336 <shw2336@naver.com>
2023-12-05 12:54:02 +0900

    이름 출력 문제 풀이

--- day02/src/welcome/information.java
+++ day02/src/welcome/information.java
@@ -3,6 +3,6 @@ package welcome;
 public class information {
 	public static void main(String[] args) {
 		System.out.println("신형우");
-		System.out.println("programing");
+		System.out.println("(디지털컨버전스)공공데이터 융합 웹 애플리케이션 개발자 양성과정7");
 	}
 }
