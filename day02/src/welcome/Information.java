cf89c04ad3e54ba4abbda3deeeaa326edc2f030a
shw2336 <shw2336@naver.com>
2023-12-05 13:09:11 +0900

    Information

---
+++ day02/src/welcome/Information.java
@@ -0,0 +1,8 @@
+package welcome;
+
+public class Information {
+	public static void main(String[] args) {
+		System.out.println("신형우");
+		System.out.println("(디지털컨버전스)공공데이터 융합 웹 이플리케이션 개발자 양성과정7");
+	}
+}
