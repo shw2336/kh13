4969a694856520baf1ce4774d725c0de55b752bf
shw2336 <shw2336@naver.com>
2023-12-07 16:04:58 +0900

    출력에 관하여

---
+++ day04/src/io/Test01출력.java
@@ -0,0 +1,13 @@
+package io; // 인풋 아웃풋 input output
+
+public class Test01출력 {
+	public static void main(String[] args) {
+		//표준 출력 통로(System.out)
+		//- 시스템의 기본 출력 장치를 이용할 수 있도록 하는 도구
+		//- println 명령은 글자 출력 후 줄바꿈(개행)을 수행
+		//- print 명령은 글자만 출력
+
+		System.out.println("Hello!");
+	}
+
+}
