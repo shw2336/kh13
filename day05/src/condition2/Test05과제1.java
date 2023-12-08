ec6088046c27e9a6da35271183835ca3bbb9086a
shw2336 <shw2336@naver.com>
2023-12-09 01:35:42 +0900

    과제 	12

--- day05/src/condition2/Test05과제1.java
+++ day05/src/condition2/Test05과제1.java
@@ -1,37 +1,38 @@
 package condition2;
 
+import java.util.Scanner;
+
 public class Test05과제1 {
 	public static void main(String[] args) {
 		
-		int moment = 10;
-		int second = 60;
-		int hour = second * second;
-		int day = hour * hour * 12;
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("초를 입력하세요 :");
+		int time = sc.nextInt();
 		
-		int time = 500;
+		//moment = 10;
+		//second = 60;
+		//minute = time / 60;
+		//hour = time / 60 / 60;
+		//day = time / 60 / 60 / 12;
 		
-		int minute = time / 60;
-				
-		//if( time < 10 ) {
-		//	System.out.println(방금 전);
-	//	}
-		//else if (10 <= time < 60) {
-		//	System.out.println("time초 전");
-	//	}
-		//else if (10 <= time < 60) {
-		//	System.out.println("time초 전");
-	//	}
-		//else if (60 <= time < 3600) {
-	//	System.out.println("?분전");
-	//	}
-	//	else if (3600 <= time < 하루) {
-	//		System.out.println("?시간전");
-	//	}
-	//	else {
-		//	System.out.println("?일전");
-	//	}
-		
+		int times;
+		if( time < 10 ) {
+			System.out.println("방금 전");
+		}
+		else if (time < 60) {
+			System.out.println(time + "초 전");
+		}
+		else if (time < 60 * 60) {
+			times = time / 60;
+		System.out.println(times + "분 전");
+		}
+		else if (time < 60 * 60 * 12) {
+			times = time / 60 / 60;
+			System.out.println(times + "시간 전");
+		}
+		else {
+			times = time / 60 / 60 / 12;
+			System.out.println(times + "일 전");
+		}
 	}
-
 }
---
+++ day05/src/condition2/Test05과제2.java
@@ -0,0 +1,9 @@
+package condition2;
+
+
+public class Test05과제2 {
+	public static void main(String[] args) {
+		
+
+		}
+	}
