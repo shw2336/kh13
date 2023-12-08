736158bb3c1fd87857f033f0c5db722327c39bf1
shw2336 <shw2336@naver.com>
2023-12-08 18:07:56 +0900

    과제

--- day05/src/condition2/Test05과제1.java
+++ day05/src/condition2/Test05과제1.java
@@ -13,24 +13,24 @@ public class Test05과제1 {
 		
 		int minute = time / 60;
 				
-		if( time < 10 ) {
-			System.out.println(방금 전);
-		}
-		else if (10 <= time < 60) {
-			System.out.println("time초 전");
-		}
-		else if (10 <= time < 60) {
-			System.out.println("time초 전");
-		}
-		else if (60 <= time < 3600) {
-			System.out.println("?분전");
-		}
-		else if (3600 <= time < 하루) {
-			System.out.println("?시간전");
-		}
-		else {
-			System.out.println("?일전");
-		}
+		//if( time < 10 ) {
+		//	System.out.println(방금 전);
+	//	}
+		//else if (10 <= time < 60) {
+		//	System.out.println("time초 전");
+	//	}
+		//else if (10 <= time < 60) {
+		//	System.out.println("time초 전");
+	//	}
+		//else if (60 <= time < 3600) {
+	//	System.out.println("?분전");
+	//	}
+	//	else if (3600 <= time < 하루) {
+	//		System.out.println("?시간전");
+	//	}
+	//	else {
+		//	System.out.println("?일전");
+	//	}
 		
 	}
 
