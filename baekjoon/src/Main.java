7862f5b2150ee6a16fd98f9ebebe302e9d53fa84
shw2336 <shw2336@naver.com>
2023-12-11 09:37:36 +0900

    백준문제풀이

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -6,18 +6,14 @@ public class Main {
 		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
+		int c = sc.nextInt();
+
 		
-		int c = a + b;
-		int d = a - b;
-		int e = a * b;
-		int f = a / b;
-		int g = a % b;
-		
-		System.out.println(c);
-		System.out.println(d);
-		System.out.println(e);
-		System.out.println(f);
-		System.out.println(g);
+	//	System.out.println((A+B)%C);
+		//System.out.println(((A%C) + (B%C))%C);
+	//	System.out.println(e);
+	//	System.out.println(f);
+		//System.out.println(g);
 		System.out.println("Hello");
 	}
 }
