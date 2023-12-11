521d95435bf76ffdb517ea9f36fa98416bab65d7
shw2336 <shw2336@naver.com>
2023-12-11 20:18:36 +0900

    구구단과제

--- day06/src/loop/Test10구구단.java
+++ day06/src/loop/Test10구구단.java
@@ -4,35 +4,24 @@ import java.util.Scanner;
 
 public class Test10구구단 {
 	public static void main(String[] args) {
-		//구구단 : (3)
-		//3 x 1 = (3)
-		//정답!
-		//3 x 2 = (5)
-		//땡!
-	//	3 x 3 = (6)
-		//정답!
-		//...
-		//3 x 9 = (27)
-	//	정답!
-	//게임 끝!
 		
-Scanner sc = new Scanner(System.in);		
-		int googoodan = sc.nextInt();
+Scanner sc = new Scanner(System.in);	
+System.out.print("단을 입력하세요 : ");
+		int number = sc.nextInt();
 		
-		for(int i = 1; i<100; i++) {
-			int count = i * googoodan;
+		for(int i = 1; i <=9; i++) {
+			System.out.print(number + "X" + i + " = ");
 			
-			System.out.println(googoodan * i);
+			int correct = number * i;
+			int wirte = sc.nextInt();
 			
-			if(googoodan % i == 0) {
-
-				System.out.println(googoodan + "X" + i +"=" + "(" + count + ")");
+			if(correct == wirte) {
 				System.out.println("정답!");
-				}
+			}
 			else {
-				System.out.println(googoodan + "X" + i +"=" + "(" +i+googoodan + ")");
 				System.out.println("땡!");
 			}
 		}
+		System.out.println("게임 끝!");
 	}
 }
