115cc118ce21b620bfd7ad8036e272fbe25bf81d
shw2336 <shw2336@naver.com>
2023-12-11 12:45:26 +0900

    백준

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -4,14 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-
-		System.out.println((a+b)%c);
-		System.out.println(((a%c) + (b%c))%c);
-		System.out.println((a*b)%c);
-		//System.out.println((a%c) * (b%c))%c();
 		
+		String a = sc.nextLine();
+		
+		System.out.println(a + "??!");
 	}
 }
