1484c888002ca0e3b96aa53f77be18bd544b2d18
shw2336 <shw2336@naver.com>
2023-12-11 17:03:03 +0900

    감소하는 반복문

--- day06/src/loop/Test08감소하는반복.java
+++ day06/src/loop/Test08감소하는반복.java
@@ -3,6 +3,9 @@ package loop;
 public class Test08감소하는반복 {
 	public static void main(String[] args) {
 		//(Q) 10부터 0까지 줄어드는 카운트다운 출력
-		
+		//for(int i=10; i>=0; i=i-1) 같은거임
+		for(int i=10; i>=0; i--) {
+			
+		}
 	}
 }
