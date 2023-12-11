cb7c1989c5c192f317eb981e46ebf9656219603d
shw2336 <shw2336@naver.com>
2023-12-11 20:34:22 +0900

    구구단

---
+++ day06/src/loop/Test10구구단2.java
@@ -0,0 +1,27 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test10구구단2 {
+	public static void main(String[] args) {
+
+		System.out.println("입력하세요");
+Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		
+		for(int i=9; i>0; i--) {
+			System.out.println(number + "X" + i + "=");
+			int right = sc.nextInt();
+			int real = number * i;
+			if(real == right) {
+				 System.out.println("정답입니다");
+			}
+			else{
+				 System.out.println("오답입니다");
+			}
+			
+		}
+		 System.out.println("끝");
+	
+	}
+}
