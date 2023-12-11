dfed1bb43a416835269f2c103f5974eb2372b4ee
shw2336 <shw2336@naver.com>
2023-12-11 17:51:26 +0900

    타이머

--- day06/src/loop/Test08감소하는반복.java
+++ day06/src/loop/Test08감소하는반복.java
@@ -2,10 +2,13 @@ package loop;
 
 public class Test08감소하는반복 {
 	public static void main(String[] args) {
-		//(Q) 10부터 0까지 줄어드는 카운트다운 출력
+		//(Q) 10부터 0까지 줄어드는 카운트다운 출력하세요
+		// - 범위 조심
+		// - 증가가 아닌 감소 처리를 해야함
+		
 		//for(int i=10; i>=0; i=i-1) 같은거임
 		for(int i=10; i>=0; i--) {
-			
+			System.out.println("good");
 		}
 	}
 }
---
+++ day06/src/loop/Test09타이머2.java
@@ -0,0 +1,40 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test09타이머2 {
+	public static void main(String[] args) {
+		//사용자에게 분과 초를 입력받아서 해당하는 시간만큼의 메세지를 출력하는 프로그램을 구현
+		// 분 입력 : 1
+		// 초 입력 : 30
+		
+		// 1분 30초 후 알람이 울립니다
+		// 1분 29초 후 알람이 울립니다
+		// 1분 28초 후 알람이 울립니다
+		// ....
+		// 0분 2초 후 알람이 울립니다
+		// 0분 1초 후 알람이 울립니다
+		// 0분 0초 후 알람이 울립니다
+		// 띠링!
+		Scanner sc = new Scanner(System.in);
+		
+		int minute = sc.nextInt();
+		int second = sc.nextInt();
+		
+		int time = minute * 60 + second;
+
+		for(int i = time; i>=0; i--) {
+			
+			if(i <= 90 && i > 0) {
+				
+				int minute1 = i / 60;
+				int second1 = i % 60;
+				
+				System.out.println(  minute1 + "분" + second1 + "초");
+			}
+			else{
+				System.out.println("띠링!");
+			}
+		}
+	}
+}
