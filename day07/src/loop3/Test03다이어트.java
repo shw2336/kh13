23bf3e66457055d77f2918a46344bbb55d5839a7
shw2336 <shw2336@naver.com>
2023-12-12 13:59:23 +0900

    다이어트

---
+++ day07/src/loop3/Test03다이어트.java
@@ -0,0 +1,20 @@
+package loop3;
+
+public class Test03다이어트 {
+	public static void main(String[] args) {
+//		오늘부터 다이어트를 위해 줄넘기를 하려고 합니다.
+//		첫날은 힘드니까 100개만 하고 다음날부터는 전날보다 10개씩 더 하려고 합니다.
+//		30일동안 하게 되는 일자별 줄넘기 개수 출력
+//		30일동안 하게 되는 총 줄넘기 개수 출력
+		
+		int first = 100;
+		int count = 1;
+		for(int i=1; i<=5; i++) {
+			
+			count += 10+i;
+		}
+		System.out.println(count);
+		System.out.println(first + count);
+	}
+
+}
