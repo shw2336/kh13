8249dd8b2aa1505208e1ea02a49f42f79e2bbfb0
shw2336 <shw2336@naver.com>
2023-12-12 14:37:15 +0900

    다이어트 파이팅

--- day07/src/loop3/Test03다이어트2.java
+++ day07/src/loop3/Test03다이어트2.java
@@ -1,22 +1,19 @@
 package loop3;
 
-public class Test03다이어트 {
+public class Test03다이어트2 {
 	public static void main(String[] args) {
 //		오늘부터 다이어트를 위해 줄넘기를 하려고 합니다.
 //		첫날은 힘드니까 100개만 하고 다음날부터는 전날보다 10개씩 더 하려고 합니다.
 //		30일동안 하게 되는 일자별 줄넘기 개수 출력
 //		30일동안 하게 되는 총 줄넘기 개수 출력
 		
-
-		int first = 100;
-		int count = 0;
-		for(int i=100; i<=30; i= i+10) {
-			
-			count += i +10 - i;
-		}
+		int begin = 100;
+		int step = 10;
 		
-		System.out.println(count - 10);
-		System.out.println(first + count - 10);
-	}
+		for(int day=1; day<=30; day++) {
+			int count = day * step + begin- 10; //첫날 100개니까 +90
+			System.out.println(day + "일차 " + "?개");
+		}
 
+	}
 }
