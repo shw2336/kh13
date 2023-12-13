4b14d448431d574d2e6b40d35922dd94c2099d12
shw2336 <shw2336@naver.com>
2023-12-13 13:35:03 +0900

    Test05 베스킨라빈스

---
+++ day08/src/loop3/Test05베스킨라빈스31.java
@@ -0,0 +1,49 @@
+package loop3;
+
+import java.util.Scanner;
+
+public class Test05베스킨라빈스31 {
+	public static void main(String[] args) {
+//		베스킨라빈스31 게임은 사용자들이 번갈아서 숫자를 1~3개 골라 31을 말하게 되는 사람이 지는 게임입니다.
+//		사용자에게 반복적으로 1~3 사이의 숫자를 입력받아 다음과 같이 게임이 진행될 수 있도록 구현해보세요
+		
+//		현재 숫자 : 1
+//		숫자 개수(1~3) 입력 : 2
+
+//		현재 숫자 : 3
+//		숫자 개수(1~3) 입력 : 3
+
+//		현재 숫자 : 6
+//		숫자 개수(1~3) 입력 : 1
+
+//		현재 숫자 : 7
+//		숫자 개수(1~3) 입력 : ?
+
+//		...
+
+//		현재 숫자 : 31
+//		숫자 개수(1~3) 입력 : 1
+
+//		당신은 패배했습니다.
+		Scanner sc = new Scanner(System.in);
+
+		int count = 0;
+		int total = 0;
+		while(true) {
+			System.out.print("숫자를 입력하세요 : ");
+			int a = sc.nextInt();
+			count += a;
+			if (count < 31) {
+			System.out.println("현재 숫자 : " + count);	
+			}
+
+			if(count == 31) {
+				System.out.println("현재 숫자 : " + count);
+				System.out.println("당신은 패배했습니다");
+				break;
+			}
+		}
+		//System.out.println(count);
+	}
+
+}
