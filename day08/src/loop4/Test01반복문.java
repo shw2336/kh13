44102407f63f23942cbf4a1ebd2f5a886e6a9643
shw2336 <shw2336@naver.com>
2023-12-13 15:10:53 +0900

    반복문!

--- day08/src/loop4/Test01반복문.java
+++ day08/src/loop4/Test01반복문.java
@@ -1,8 +1,23 @@
 package loop4;
 
+import java.util.Scanner;
+
 public class Test01반복문 {
 	public static void main(String[] args) {
+//		do-while 반복
+//		선 실행(do)후 반복(while)의 형태를 가지는 구문
+//		(ex) 시험 점수를 제대로 입력할 때까지 입력받는 코드
+		Scanner sc = new Scanner(System.in);
 		
+		int score;
+		
+		do {//반복 실행할 내용
+			System.out.println("시험점수 입력 : ");
+			score = sc.nextInt();
+		}
+//		while(score < 0 || score > 100);
+		while(!(score >= 0 && score <= 100));
+		
+		System.out.println("입력된 점수 = " + score);
 	}
-
 }
