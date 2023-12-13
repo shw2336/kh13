d0001f4d3eedca4a28696af617ee91bee6d53fd5
shw2336 <shw2336@naver.com>
2023-12-13 10:16:42 +0900

    랜덤구구단

--- day07/src/random/Test03랜덤구구단완성형.java
+++ day07/src/random/Test03랜덤구구단완성형.java
@@ -5,46 +5,40 @@ import java.util.Scanner;
 
 public class Test03랜덤구구단완성형 {
 	public static void main(String[] args) {
-		//10개의 랜덤 구구단 문제를 출제하는 프로그램을 구현하세요
-		//2단 부터 19단 까지의 범위 내에서 출제
-		//사용자에게 입력을 받아 정답, 오답 판정
-		//사용자가 맞춘 문제 수를 카운트하여 출력
-		//맞춘 문제 하나당 점수를 부여
-		//쉬운 문제는 10점 (2~11단, x1)
-		//어려운 문제는 15점 (나머지)
-		//사용자가 획득한 점수를 누적합산하여 출력
-		
+
 		Random r = new Random();
 		Scanner sc = new Scanner(System.in);
-		int count = 0;
-		int counthard = 0;
-		int counteasy = 0;
-		for(int i=1; i<=10; i++) {
-			int a = r.nextInt(18) + 2;
-			int b = r.nextInt(19) + 1;
-			int answer = a * b;
-			System.out.print(a + " X " + b + "= " );
-			int write = sc.nextInt();
-			if(answer == write) {System.out.println("정답입니다!");
-				count ++;
-				if(a>11 || b > 11) {
-					counthard ++;
+
+		int size = 10;
+		int correctEasy = 0, correctDifficult = 0;//개수
+		int score = 0;//점수
+
+		for(int i=1; i <= size; i++) {
+			int left = r.nextInt(18) + 2;//2부터 18개//2부터 19사이
+			int right = r.nextInt(9) + 1;//1부터 9개//1부터 9사이
+
+			System.out.print(left + " x " + right + " = " );
+			int user = sc.nextInt();
+
+			if(user == left * right) {//정답이라면
+
+				//어려운 문제라면 - 12단 이상이면서 x2 이상
+				if(left >= 12 && right >= 2) {
+					//System.out.println("어려운 문제 클리어");
+					correctDifficult++;
+					score += 15;
 				}
+				//쉬운 문제라면 - 11단 이하이거나 x1인 경우
 				else {
-					counteasy ++;
+					//System.out.println("쉬운 문제 클리어");
+					correctEasy++;
+					score += 10;
 				}
-			}//
-			else {
-				System.out.println("오답입니다!");
 			}
 		}
-		int hardscore = counthard * 15;
-		int easyscore = counteasy * 10;
-		int all = hardscore + easyscore;
-		System.out.println("정답은 " + count + "개");
-		System.out.println("오답은 " + (10 - count) + "개");
-		System.out.println("쉬운 문제 " + counteasy + "개" + " 점수는 " + easyscore + "입니다");
-		System.out.println("어려운 문제 " + counthard + "개" + " 점수는 " + hardscore + "입니다");
-		System.out.println("총 점수는 " + all + "입니다");	
+		System.out.println("게임 끝!");
+		System.out.println("쉬운 문제 정답 개수 : " + correctEasy);
+		System.out.println("어려운 문제 정답 개수 : " + correctDifficult);
+		System.out.println("총 획득 점수 : " + score+"점");
 	}
 }
--- day07/src/random/Test03랜덤구구단콤보만들기2.java
+++ day07/src/random/Test03랜덤구구단콤보만들기2.java
@@ -5,46 +5,36 @@ import java.util.Scanner;
 
 public class Test03랜덤구구단콤보만들기2 {
 	public static void main(String[] args) {
-		//10개의 랜덤 구구단 문제를 출제하는 프로그램을 구현하세요
-		//2단 부터 19단 까지의 범위 내에서 출제
-		//사용자에게 입력을 받아 정답, 오답 판정
-		//사용자가 맞춘 문제 수를 카운트하여 출력
-		//맞춘 문제 하나당 점수를 부여
-		//쉬운 문제는 10점 (2~11단, x1)
-		//어려운 문제는 15점 (나머지)
-		//사용자가 획득한 점수를 누적합산하여 출력
-		
+		//콤보도 결국 카운트
+		//- 정답을 맞추면 1 증가
+		//- 틀리면 0
+
 		Random r = new Random();
 		Scanner sc = new Scanner(System.in);
-		int count = 0;
-		int counthard = 0;
-		int counteasy = 0;
-		for(int i=1; i<=10; i++) {
-			int a = r.nextInt(18) + 2;
-			int b = r.nextInt(19) + 1;
-			int answer = a * b;
-			System.out.print(a + " X " + b + "= " );
-			int write = sc.nextInt();
-			if(answer == write) {System.out.println("정답입니다!");
-				count ++;
-				if(a>11 || b > 11) {
-					counthard ++;
+
+		int size = 10;
+		int combo = 0;
+
+		for(int i=1; i <= size; i++) {
+			int left = r.nextInt(18) + 2;//2부터 18개//2부터 19사이
+			int right = r.nextInt(9) + 1;//1부터 9개//1부터 9사이
+
+			System.out.print(left + " x " + right + " = " );
+			int user = sc.nextInt();
+
+			if(user == left * right) {//정답이라면
+				combo++;
+				if(combo >= 2) {
+					System.out.println(combo + " Combo!");
 				}
-				else {
-					counteasy ++;
-				}
-			}//
-			else {
-				System.out.println("오답입니다!");
+			}
+			else {//오답이라면
+				combo = 0;
 			}
 		}
-		int hardscore = counthard * 15;
-		int easyscore = counteasy * 10;
-		int all = hardscore + easyscore;
-		System.out.println("정답은 " + count + "개");
-		System.out.println("오답은 " + (10 - count) + "개");
-		System.out.println("쉬운 문제 " + counteasy + "개" + " 점수는 " + easyscore + "입니다");
-		System.out.println("어려운 문제 " + counthard + "개" + " 점수는 " + hardscore + "입니다");
-		System.out.println("총 점수는 " + all + "입니다");	
+
+
+		System.out.println("게임 끝!");
+
 	}
 }
