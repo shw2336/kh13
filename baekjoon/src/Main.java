a291785bc92ea9c44c6b6c6d010fe1557d5dc572
shw2336 <shw2336@naver.com>
2023-12-13 15:17:19 +0900

    백준어렵다

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -4,12 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 Scanner sc = new Scanner(System.in);	
-		int T = sc.nextInt();	
+		int hour = 3;
+		int minute = hour * 60;
 		
-		for(int i=1; i<=T; i++) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			System.out.println(A + B);
+		
 		}
 	}
-}
