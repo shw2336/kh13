d3e42e200f4d3007251164f4b54c6c0e91fccaa3
shw2336 <shw2336@naver.com>
2023-12-13 10:16:59 +0900

    반복문과 무한반복

---
+++ day08/src/loop3/Test01반복문.java
@@ -0,0 +1,20 @@
+package loop3;
+
+public class Test01반복문 {
+	public static void main(String[] args) {
+		//두 번째 반복문 - while 반복문
+		
+		//1부터 10까지 출력
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println();
+		
+		int n=1;
+		while(n <=10) {
+			System.out.println(n);
+			n++;
+		}
+	}
+}
---
+++ day08/src/loop3/Test02무한반복.java
@@ -0,0 +1,7 @@
+package loop3;
+
+public class Test02무한반복 {
+	public static void main(String[] args) {
+		
+	}
+}
