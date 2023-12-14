ce964d1ccb35b05818456725a2288987db8f1e75
shw2336 <shw2336@naver.com>
2023-12-14 20:23:48 +0900

    배열섞기과제

--- day09/src/array2/Test06배열섞기.java
+++ day09/src/array2/Test06배열섞기.java
@@ -12,13 +12,17 @@ public class Test06배열섞기 {
 		바뀌는 자리가 같은 자리여도 상관없음
 		모든 자리가 교체된 후 배열을 출력*/
 		
-		
-		
 		Random r = new Random();
 		int[] numbers = new int[] {30, 50, 20, 10, 40};
-		for(int i=1; i<numbers.length/2; i++) {
-			int number = r.nextInt();
-			
+		
+		for(int i=0; i<numbers.length; i++) {
+			int random = r.nextInt(numbers.length);
+//			System.out.println(random);
+			int backup = numbers[i];
+			numbers[i] = numbers[random];
+			numbers[random] = backup;		
+		}
+		for(int i=0; i<numbers.length; i++) {
 			System.out.println(numbers[i]);
 		}
 	}
