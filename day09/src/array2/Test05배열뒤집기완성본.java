e3aebd5814bdd08d7cf9225627f913f3a7f9e080
shw2336 <shw2336@naver.com>
2023-12-14 18:23:25 +0900

    배열과제

--- day09/src/array2/Test05배열뒤집기완성본.java
+++ day09/src/array2/Test05배열뒤집기완성본.java
@@ -2,48 +2,49 @@ package array2;
 
 public class Test05배열뒤집기완성본 {
 	public static void main(String[] args) {
-		
-//		30, 50, 20, 10, 40을 배열에 저장하고 이 배열의 모든 데이터의 위치를 반대로 뒤집은 뒤 출력하세요
-		
+
 		//배열 준비
 		int[] numbers = new int[] {30, 50, 20, 10, 40};
-		
+
 		/*
-		 위치 교체
-		 -데이터의 개수에 따른 교체 규칙을 살펴보자!
-		 
-		 데이터 1개		교체0번		{30}
-		 데이터 2개	교체1번		{30,50}
-		 									numbers[0] ↔numbers[1]
-		 데이터 3개	교체1번		{30,50,20}
-		 									numbers[0] ↔numbers[2]
-		 데이터 4개	교체2번		{30,50,20,10}
-		 									numbers[0] ↔numbers[3]
-		 									numbers[1] ↔numbers[2]
-		 데이터 5개	교체2번		{30,50,20,10,40}  		     	//데이터 개수 나누기 2 는 교체하는 횟수
-		 									numbers[0] ↔numbers[4]
-		 									numbers[1] ↔numbers[3]
-		 									
-		 데이터 n개 	교체n/2번  numbers[0] ↔numbers[n-1]
-		 									numbers[0] ↔numbers[n-2]
-		 */
 		
-		int backup = numbers[0];
-		numbers[0] = numbers[4];
-		numbers[4] = backup;
+		위치 교체
+		- 데이터의 개수에 따른 교체 규칙을 살펴보자!
+		
+		데이터 1개 		교체0번			{30}
+		데이터 2개		교체1번			{30,50}
+												numbers[0] ↔ numbers[1]
+		데이터 3개		교체1번			{30,50,20}
+												numbers[0] ↔ numbers[2]
+		데이터 4개		교체2번			{30,50,20,10}
+												numbers[0] ↔ numbers[3]
+												numbers[1] ↔ numbers[2]
+		데이터 5개		교체2번			{30,50,20,10,40}
+												numbers[0] ↔ numbers[4]
+												numbers[1] ↔ numbers[3]
+		
+		데이터 n개		교체 n/2번		numbers[0] ↔ numbers[n-1]
+												numbers[1] ↔ numbers[n-2]
+												...
+		*/
 
-		backup = numbers[1];
-		numbers[1] = numbers[3];
-		numbers[3] = backup;
-		
-		int left = 0;
-		//nt right = 
-		//for(int)
-		for(int i=0; i<numbers.length; i++) {
+		int left = 0;//처음 위치
+		int right = numbers.length-1;//마지막 위치
+//		for(int i=0; i < numbers.length/2; i++) {
+		for(int i=1; i <= numbers.length/2; i++) {//데이터 개수 절반만큼 반복
+			//left위치와 right위치를 바꾸면된다
+			int backup = numbers[left];
+			numbers[left] = numbers[right];
+			numbers[right] = backup;
+
+			//다음 턴을 위해서 left는 1증가, right는 1감소 처리
+			left++;
+			right--;
+		}
+
+		//출력
+		for(int i=0; i < numbers.length ; i++) {
 			System.out.println(numbers[i]);
-			}
-
-		
+		}
 	}
-
 }
---
+++ day09/src/array2/Test06배열섞기.java
@@ -0,0 +1,25 @@
+package array2;
+
+import java.util.Random;
+
+public class Test06배열섞기 {
+	public static void main(String[] args) {
+		
+		/*Test05와 동일한 데이터를 배열에 저장하고 다음 규칙에 따라 데이터를 섞어보세요
+
+		실행할 때마다 다른 위치로 바뀌도록 섞고 싶음
+		배열의 모든 자리가 1번은 반드시 바뀌어야함
+		바뀌는 자리가 같은 자리여도 상관없음
+		모든 자리가 교체된 후 배열을 출력*/
+		
+		
+		
+		Random r = new Random();
+		int[] numbers = new int[] {30, 50, 20, 10, 40};
+		for(int i=1; i<numbers.length/2; i++) {
+			int number = r.nextInt();
+			
+			System.out.println(numbers[i]);
+		}
+	}
+}
