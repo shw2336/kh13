880bcb0671979f38ab1f3578e0dc87732ce79022
shw2336 <shw2336@naver.com>
2023-12-15 13:05:35 +0900

    선택정렬

--- day10/src/array3/Test01선택정렬완성.java
+++ day10/src/array3/Test01선택정렬완성.java
@@ -2,24 +2,31 @@ package array3;
 
 public class Test01선택정렬완성 {
 	public static void main(String[] args) {
-		
+
+		//배열 준비
 		int[] numbers = new int[] {30, 50, 20, 10, 40};
-		
-		
-	
-		int min = 0;
-		for(int i=1; i<numbers.length; i++) {
-			if(numbers[min] > numbers[i]) {
-				min = i;
+
+		//선택 정렬(selection sort) - 최소값을 앞에서부터 채우는 방식
+
+		//세 번째 회차에서 발생하는 일
+		//- [2]부터 [4] 사이에서 최소값의 위치를 찾아 [2]과 바꾼다
+
+		int min = 2;//[2]이 가장 작은값의 위치라고 치자
+		for(int i=3; i <= 4; i++) {//[3]부터 [4] 사이의 범위에서
+			if(numbers[min] > numbers[i]) {//내 생각보다 더 작은값이 있다면
+				min = i;//생각을 수정한다
 			}
 		}
-		
-		int backup = numbers[0];
-		numbers[0] = numbers[min];
+
+		int backup = numbers[2];
+		numbers[2] = numbers[min];
 		numbers[min] = backup;
-		
-		for(int i=0; i<numbers.length; i++) {
+
+
+		//출력
+		for(int i=0; i < numbers.length; i++) {
 			System.out.println(numbers[i]);
 		}
+
 	}
 }
--- day10/src/array3/Test01선택정렬완성3.java
+++ day10/src/array3/Test01선택정렬완성3.java
@@ -1,15 +1,15 @@
 package array3;
 
-public class Test01선택정렬완성2 {
+public class Test01선택정렬완성3 {
 	public static void main(String[] args) {
 
 		//배열 준비
 		int[] numbers = new int[] {30, 50, 20, 10, 40};
 
 		//선택 정렬(selection sort) - 최소값을 앞에서부터 채우는 방식
-		for(int k=0; k <= 3; k++) {
+		for(int k=0; k < numbers.length-1; k++) {
 			int min = k;
-			for(int i=k+1; i <= 4; i++) {//마지막까지
+			for(int i=k+1; i < numbers.length; i++) {//마지막까지
 				if(numbers[min] > numbers[i]) {
 					min = i;
 				}
