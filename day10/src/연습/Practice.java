63fd1aef7b6cd009ad7dfb84bacc33d9a26cab06
shw2336 <shw2336@naver.com>
2023-12-17 22:08:46 +0900

    practice

---
+++ day10/src/연습/Practice.java
@@ -0,0 +1,24 @@
+package 연습;
+
+public class Practice {
+	public static void main(String[] args) {
+
+		int[] numbers = new int[] { 30, 50, 20, 10, 40 };
+
+			int min=0;
+			for(int i=1; i<numbers.length; i++) {
+				if(numbers[min] > numbers[i]) {
+					min = i;
+				}
+			}
+			
+			int backup = numbers[0];
+			numbers[0] = numbers[min];
+			numbers[min] = backup;
+			
+			for(int i=0; i<numbers.length; i++) {
+				System.out.println(numbers[i]);
+				
+			}
+	}
+}
