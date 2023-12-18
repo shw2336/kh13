8db218d55cb2c95957bbe1b3e3511f478ea23a4e
shw2336 <shw2336@naver.com>
2023-12-18 13:02:45 +0900

    문제풀이

--- baekjoon/src/문제풀이기억하고싶은것2.java
+++ baekjoon/src/문제풀이기억하고싶은것2.java
@@ -6,16 +6,16 @@ public class 문제풀이기억하고싶은것2 {
 //https://www.acmicpc.net/problem/10871 문제 풀다가 대기중
 		Scanner sc = new Scanner(System.in);
 		int c = sc.nextInt();
-		int a = sc.nextInt();
 		int[] b = new int[c];
+		//int a = sc.nextInt();
 		//int[] b = new int[c] {d};
 		
 		for (int i = 0; i < b.length; i++) {
-			if (b[a] > b[i]) {
+			//if (b[a] > b[i]) {
 				System.out.println(b[i]);
 			}
 		}
-		System.out.println("\\");
+		//System.out.println("\\");
 
-		}
+		//}
 	}
