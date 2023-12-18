93adb5eb511dd61c433d54a8e61986bc8d65da20
shw2336 <shw2336@naver.com>
2023-12-18 20:36:21 +0900

    객체생성

--- day11/src/oop/method8/Test.java
+++ day11/src/oop/method8/Test.java
@@ -4,14 +4,29 @@ public class Test {
 	int grade;
 	int group;
 	String name;
-	boolean language;
-	boolean english;
-	boolean math;
-	int all;
-	boolean average;
-	
-	
-	
-	
+	int language;
+	int english;
+	int math;
+	int total;
+	double average;
 
+	void data(int grade, int group, String name, int language, int english, int math, int total, double average) {
+		this.data(grade, group, name, language, english, math, total, average);
+	}
+
+	void information() {
+		System.out.println("<학생 성적 정보>");
+		System.out.println(this.grade + "학년 " + this.group + "반 " + this.name);
+		int total = this.language + this.english + this.math;
+		double average = (double) total / 3;
+		System.out.println("국어 : " + this.language + "영어 : " + this.english + "수학 : " + this.math);
+		System.out.println("총점 : " + total + "평균 : " + average);
+
+		if (this.language < 40 || this.english < 40 || this.math < 40 || this.average < 60) {
+			System.out.println("재평가 대상자입니다");
+		} else {
+			System.out.println("축하드립니다 통과입니다");
+		}
+		System.out.println();
+	}
 }
--- day11/src/oop/method8/Test01객체생성.java
+++ day11/src/oop/method8/Test01객체생성.java
@@ -2,7 +2,19 @@ package oop.method8;
 
 public class Test01객체생성 {
 	public static void main(String[] args) {
+		Test a = new Test();
+		Test b = new Test();
+		Test c = new Test();
+		Test d = new Test();
 		
+		a.data(1, 1, "마리오", 50, 60, 50, 160, 53.333);
+		b.data(1, 1, "루이지", 60, 90, 50, 200, 66.666);
+		c.data(1, 2, "쿠파", 70, 70, 80	, 220, 73.333);
+		d.data(1, 2, "요시", 80, 85, 35, 200, 66.666);
+		
+		a.information();
+		b.information();
+		c.information();
+		d.information();
 	}
-
 }
