8df754ef72b2baf6738f09e14a7b7d964e3b72f1
shw2336 <shw2336@naver.com>
2023-12-20 17:49:15 +0900

    example

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -5,20 +5,16 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		int[] A = new int[9];
-		for (int i = 0; i < A.length; i++) {
-			A[i] = sc.nextInt();
-		}
-		int same = 0;
-		int max = 0;
-		for (int i = 1; i < A.length; i++) {
-			if (A[max] < A[i]) {
-				max = i;
-				same = i;
-			}
-		}
-
-		System.out.println(A[max]);
-		System.out.println(same + 1 );
+//		int a = sc.next();
+//		int b = sc.nextInt();
+//		
+//		System.out.println(a);
+//
+//		int b = a;
+//		System.out.println(b);
+//		
+//		char c =44032;
+//		System.out.println(c);
+		
 	}
 }
