af1ad5679e1ab6112fc44acdb1fbe31f47162c38
shw2336 <shw2336@naver.com>
2023-12-28 10:49:53 +0900

    문자열더하기 / 시스템클래스 / 문자열버퍼

---
+++ day18/src/api/lang/etc/Test01시스템클래스.java
@@ -0,0 +1,11 @@
+package api.lang.etc;
+
+public class Test01시스템클래스 {
+	public static void main(String[] args) {
+		//System 클래스
+		//- 생성자가 없는 클래스
+		//- 생성자가 없다는 것은 (1) 객체 아예 생성 불가 (2) 객체 생성 개수 제한
+		//- 모든 필드와 메소드가 static이면 (1)번에 해당
+	}
+
+}
--- day18/src/api/lang/string3/Test02문자열버퍼.java
+++ day18/src/api/lang/string3/Test02문자열버퍼.java
@@ -1,17 +1,22 @@
 package api.lang.string3;
 
-public class Test01문자열더하기 {
+public class Test02문자열버퍼 {
 	public static void main(String[] args) {
-		//문자열이 불변(immutable)이어서 생기는 단점 살펴보기(ex : 별 생성)
+		//StringBuffer를 이용한 문자열 합산
 		
-		String star = "";
+		//String star = "";
+		StringBuffer buffer = new StringBuffer();
 		long begin = System.currentTimeMillis(); //현재 시간을 밀리초(ms)로 반환
 		for(int i=1; i<=10; i++) {
-			star += "*";
+			//star += "*";
+			buffer.append("*");
 			
 		}
 		long end = System.currentTimeMillis();
 		long gap = end - begin;
+		
+		buffer.toString();
+		System.out.println(buffer);
 		System.out.println("gap = " + gap);
 		
 		
