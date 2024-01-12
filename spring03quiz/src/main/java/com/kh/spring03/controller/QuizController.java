bd30b6a22f97fee505a08c02ff6975002b6fc53d
shw2336 <shw2336@naver.com>
2024-01-12 19:03:32 +0900

    과제

--- spring03quiz/src/main/java/com/kh/spring03/controller/QuizController.java
+++ spring03quiz/src/main/java/com/kh/spring03/controller/QuizController.java
@@ -6,35 +6,39 @@ import java.util.Random;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-///dice : 랜덤으로 주사위 번호 1개를 출력
-///lotto : 랜덤으로 로또번호 6개를 출력
-///food : 랜덤으로 본인이 원하는 식사메뉴 중 하나를 출력
-
 @RestController
 public class QuizController {
-    Random r = new Random();
-    
-    @RequestMapping("/dice")
-    public int dice() {
-        int diceNum = r.nextInt(6) + 1;
-        return diceNum;
-    }
-    
-    @RequestMapping("/lotto")
-    public ArrayList<Integer> lotto() {
-        ArrayList<Integer> lotto = new ArrayList<>();
-        for(int i = 1 ; i <= 6 ; i++) {
-            int lottoNum = r.nextInt(45) + 1;
-                if(!lotto.contains(lottoNum)){
-                    lotto.add(lottoNum);
-                    }
-                else {
-                    i--;
-                }
-            }
-            return lotto;
-        }
-    
-//    @RequestMapping("/food")
-//    public 
+	Random r = new Random();
+
+	@RequestMapping("/dice")
+	public int dice() {
+
+		int dice = r.nextInt(6) + 1;
+
+		return dice;
+	}
+
+	@RequestMapping("/lotto")
+	public ArrayList<Integer> lotto() {
+		ArrayList<Integer> lotto = new ArrayList<>();
+		for (int i = 1; i <= 6; i++) {
+			int lottoNum = r.nextInt(45) + 1;
+			if (!lotto.contains(lottoNum)) {
+				lotto.add(lottoNum);
+			} else {
+				i--;
+			}
+		}
+		return lotto;
+	}
+
+	@RequestMapping("/food")
+	public String food() {
+		Random r = new Random();
+		String[] food = new String[] { "라면", "햄버거", "피자", "치킨", "보쌈" };
+		int position = r.nextInt(food.length);
+		String result = food[position];
+		return result;
+	}
+
 }
