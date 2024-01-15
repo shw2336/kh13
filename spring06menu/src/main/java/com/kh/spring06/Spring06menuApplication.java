631c37b5b5a5b3a099fee9c18b89ebe80109ffeb
shw2336 <shw2336@naver.com>
2024-01-15 16:43:52 +0900

    spring06

---
+++ spring06menu/src/main/java/com/kh/spring06/Spring06menuApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06menuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring06menuApplication.class, args);
+	}
+
+}
