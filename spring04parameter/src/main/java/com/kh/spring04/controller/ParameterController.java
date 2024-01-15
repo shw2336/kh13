6362ceedbbd8c8e1f6deaa37bddf9f7c0b02d540
shw2336 <shw2336@naver.com>
2024-01-15 13:14:11 +0900

    파라미터

--- spring04parameter/src/main/java/com/kh/spring04/controller/ParameterController.java
+++ spring04parameter/src/main/java/com/kh/spring04/controller/ParameterController.java
@@ -1,6 +1,10 @@
 package com.kh.spring04.controller;
 
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,20 +23,46 @@ public class ParameterController {
 	//(Q) 출생년도 8자리를 입력하면 한국 나이를 반환
 	@RequestMapping("/KoreanAge")
 	public String koreanAge(int birth) {
-		int now = 20240115;
-		int nowYear = now / 10000;
-		int birthYear = birth / 10000;
-		int result = nowYear - birthYear + 1;
-		return "한국 나이는 : " + result;
+		int year = birth / 10000;
+		int current = LocalDate.now().getYear();
+		int age = current - year + 1;
+		return "한국 나이는 : " + age;
 	}
 	
 	//(Q) 키(cm)와 체중(kg)을 입력하면 BMI를 반환
 	@RequestMapping("/bmi")
-	public String bmi(int height, int kg) {
-		int heightN = height / 100;
-		int result = kg / (heightN * heightN);
+	public String bmi(float cm, float kg) {
+		float m = cm / 100;
+		float result = kg / (m * m);
+		DecimalFormat fmt = new DecimalFormat("#,##0.00");
 		
-		return "BMI는 : "  + result;
+		return "BMI는 : "  + fmt.format(result);
+		
 	}
+	//파라미터를 받기 위한 고급 설정이 가능하다
+	//- price가 없을 경우 0으로 대체
+	//- people이 없을 경우 1로 대체
+	//- 파라미터에 @RequestParam 등록을 해야함
+	@RequestMapping("/dutch")
+	public String dutch(
+			@RequestParam(required = false, defaultValue = "0") int price, 
+			@RequestParam(required = false, defaultValue = "1") int people) {
+		int result = price / people;
+		return "한사람당" + result + "원";
+	}
+	
+	//(Q) /coffee라는 주소를 만들어서 다음과 같이 처리해보세요
+	//- kind와 shot이라는 이름의 파라미터를 받아서 커피명과 샷 수를 출력
+	//- kind=latte&shot=1일 경우 "latte 주문, 샷 1개 추가"처럼 페이지 출력
+	//- kind가 없으면 americano 로 처리
+	//- shot이 없으면 0으로 처리
+	@RequestMapping("/coffee")
+	public String coffee(
+			@RequestParam(required = false, defaultValue = "americano") String kind, 
+			@RequestParam(required = false, defaultValue = "0") int shot) {
+		return kind + "주문, 샷" + shot + "개 추가";
+	}
+	
+	
 	
 }
