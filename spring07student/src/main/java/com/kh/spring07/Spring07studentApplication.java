b2ad6fb7e2b727702f81353fc0b26b2fde93cb59
shw2336 <shw2336@naver.com>
2024-01-16 16:59:28 +0900

    spring07

---
+++ spring07student/src/main/java/com/kh/spring07/Spring07studentApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07studentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07studentApplication.class, args);
+	}
+
+}
