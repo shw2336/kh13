bf32864fcafdda6bc9befb25825f1dcfbf5e1cba
shw2336 <shw2336@naver.com>
2024-01-17 12:44:59 +0900

    spring09 github

--- spring08jsp/src/main/java/com/kh/spring08/controller/HomeController.java
+++ spring08jsp/src/main/java/com/kh/spring08/controller/HomeController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-//@RestController
+@Controller//결과를 화면으로 반환하는 컨트롤러
+//@RestController//결과를 직접 생성하는 컨트롤러
 public class HomeController {
 
 	@RequestMapping("/welcome")
 	public String welcome() {
 		//return "<h1> Welcome<h1>"; //글자 진하게 보여줌!
-		return "/WEB-INF/views/welcome.jsp";
+		return "/WEB-INF/views/welcome.jsp";//화면 주소
 	}
 }
