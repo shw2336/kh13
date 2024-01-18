68a67110e53d7b321bec60ae6d9078fc5ee5033c
shw2336 <shw2336@naver.com>
2024-01-18 17:18:26 +0900

    edit

--- spring10integrated/src/main/java/com/kh/spring10/controller/EmpController.java
+++ spring10integrated/src/main/java/com/kh/spring10/controller/EmpController.java
@@ -2,10 +2,12 @@ package com.kh.spring10.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.spring10.dao.EmpDao;
 import com.kh.spring10.dto.EmpDto;
@@ -37,8 +39,17 @@ public class EmpController {
 		return "/WEB-INF/views/emp/insert3.jsp";
 	}
 	
+	
+	//- 수정페이지
+	//- 수정페이지는 일반적으로 상세/목록에서 들어온다
+	//- 처음에 정보가 다 표시되어 있어야 한다
+	//- 그러기 위해서는 출력페이지에서 기본키(PK)정도는 전달받아야 한다
+	//- 무언가를 화면으로 전달하고 싶다면 매개변수에 Model 객체를 선언
+	//- model은 Map<String, Object> 형태
 	@GetMapping("/edit")
-	public String edit() {
+	public String edit(@RequestParam int empNo, Model model) {
+		EmpDto dto = dao.selectOne(empNo);
+		model.addAttribute("dto", dto);
 		return "/WEB-INF/views/emp/edit.jsp";
 	}
 	@PostMapping("/edit")
--- spring10integrated/src/main/webapp/WEB-INF/views/emp/edit.jsp
+++ spring10integrated/src/main/webapp/WEB-INF/views/emp/edit.jsp
@@ -1,13 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 
-    <h1>사원 정보 수정</h1>
+
+    <h1>사원 정보 변경</h1>
     
     <form action="edit" method="post">
-    	번호 <input type="text" name="empNo" required> <br><br>
-    	이름 <input type="text" name="empName" required> <br><br>
-    	부서 <input type="text" name="empDept" required> <br><br>
-    	입사일 <input type="date" name="empDate" required> <br><br>
-    	급여 <input type="number" name="empSal" required> <br><br>
+    	 <input type="hidden" name="empNo" required value="${dto.empNo}">
+    	이름 <input type="text" name="empName" required value="${dto.empName}"> <br><br>
+    	부서 <input type="text" name="empDept" required value="${dto.empDept}"> <br><br>
+    	입사일 <input type="date" name="empDate" required value="${dto.empDate}"> <br><br>
+    	급여 <input type="number" name="empSal" required value="${dto.empSal}"> <br><br>
     	<button>정보 수정</button>
     </form>
