0a21e8cb0f523bee13e18b0aa07f682cdea126bb
shw2336 <shw2336@naver.com>
2024-02-06 15:27:14 +0900

    dd

--- spring10integrated/src/main/java/com/kh/spring10/controller/HomeController.java
+++ spring10integrated/src/main/java/com/kh/spring10/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class HomeController {//dd
 	//메인페이지는 가장 짧은 주소로 서비스
 	//-공용 주소를 부여하지 않고 /로 주소를 설정
 	
