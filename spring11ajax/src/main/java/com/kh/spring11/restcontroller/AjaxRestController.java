19942f6f1c487196f6298a3f252bdfaac7861084
shw2336 <shw2336@naver.com>
2024-02-27 10:04:19 +0900

    dd

---
+++ spring11ajax/src/main/java/com/kh/spring11/restcontroller/AjaxRestController.java
@@ -0,0 +1,11 @@
+package com.kh.spring11.restcontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller // 사용자에게 화면을 전송하는 컨트롤러
+@RestController //사용자에게 데이터를 전송하는 컨트롤러(화면x)
+public class AjaxRestController {
+	
+	
+
+}
