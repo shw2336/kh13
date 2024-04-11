850a356875ea40f1168be0e421ab87a8922bb7f2
shw2336 <shw2336@naver.com>
2024-04-11 13:51:53 +0900

    spring18올리고

---
+++ spring18websocket/src/main/java/com/kh/spring18/Spring18websocketApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring18websocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring18websocketApplication.class, args);
+	}
+
+}
